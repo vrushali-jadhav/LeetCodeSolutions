85d32f0d3e26d35e28985637123db5fb09e6cca0
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-02-06 15:46:57 -0800

    Add files via upload

---
+++ searchInsertPosition.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return i+1
+
+s = Solution()
+print(s.searchInsert([1,3,5,6],2))
