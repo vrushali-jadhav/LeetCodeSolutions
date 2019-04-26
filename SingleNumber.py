603243de8494d02fc3a3e215570b3048df2676a2
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-04-26 00:59:44 -0700

    Update SingleNumber.py

--- SingleNumber.py
+++ SingleNumber.py
@@ -8,6 +8,20 @@ class Solution(object):
         dict1 = sorted(collections.Counter(nums).items(), key = lambda t:t[1])
         tuple1 = dict1[0]
         return tuple1[0]
+    
+        /*
+        dict1 = {}
+        
+        for num in nums:
+            if num not in dict1:
+                dict1[num] = 1
+            else:
+                dict1[num] += 1
+                
+        dict1 = sorted(dict1.items(), key= lambda t:t[1])
+        tuple1 = dict1[0]
+        return tuple1[0]
+        */
         
 s = Solution()
 print(s.singleNumber([1,1,2,2,1]))
