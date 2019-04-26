6eacdba5af5ded9bc9d6dcc2a362a37c2a85ce82
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-04-26 00:10:33 -0700

    Update maximumSubArray.py

--- maximumSubArray.py
+++ maximumSubArray.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        total = 0
-        maxTotal = 0
-        startIndex = 0 
-        endIndex = 0
-
-        for num in nums:
-            total = max(total+num,total)
-            
-            if total > maxTotal:
-                maxTotal = total
-
-        return maxTotal
+        if max(nums) < 0:
+            return max(nums)
+        global_max, local_max = 0, 0
+        for x in nums:
+            local_max = max(0, local_max + x)
+            global_max = max(global_max, local_max)
+        return global_max
 
 s = Solution()
 print(s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
