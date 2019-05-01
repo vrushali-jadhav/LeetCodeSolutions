4e4816bd9e61dcc0c66e1375d7b63fcf10ad6a3f
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-05-01 13:02:50 -0700

    Update MinimumWindowSubstring.py

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -7,25 +7,20 @@ class Solution(object):
         :rtype: str
         """
         #to reduce the counts if char is found
-        count1={} 
-        #to keep matching if the char is in the dict
-        count2={}
+        dictForCount={} 
+        dictForCount = dict(collections.Counter(T))
         
-        count1 = dict(collections.Counter(T))
-        count2 = dict(collections.Counter(T))
-
         #keep decreasing the count. if all elements from right string are found, count would be 0
         count=len(T)
         start, minSize, minStart = 0, float("inf"), 0
         
         for indx, val in enumerate((S)):
-        
-            if val in count2 and count2[val]>0:
+            if val in dictForCount:
                 #this will also set values to <0 when you encounter the char again 
-                count1[val]-=1
+                dictForCount[val]-=1
                 
                 #found the element, so decrease the count
-                if count1[val]>=0:
+                if dictForCount[val]>=0:
                     count-=1
             
             #the first time reach here, all the values in count are set to 0
@@ -33,11 +28,12 @@ class Solution(object):
             #now we have to see if the can find the chars again
             if count==0:
                 while True:
-                    if S[start] in count2:
-                        if count1[S[start]]<0:
-                            count1[S[start]]+=1
+                    if S[start] in dictForCount:
+                        if dictForCount[S[start]]<0:
+                            dictForCount[S[start]]+=1
                         else:
                             break
+                    #increment start it points to a char that is in the dict
                     start+=1
                 if minSize>indx-start+1:
                     minSize=indx-start+1 
@@ -50,4 +46,4 @@ class Solution(object):
         
 s = Solution()
 
-print(s.minWindow("ADOBECODEBANC","ABC"))
+print(s.minWindow("dbcab","ab"))
