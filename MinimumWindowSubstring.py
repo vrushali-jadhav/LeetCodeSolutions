513e7a323383bc030d7a0ef0e97983956dcf4a94
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-05-01 13:22:46 -0700

    Update MinimumWindowSubstring.py

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -6,33 +6,30 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        #to reduce the counts if char is found
         dictForCount={} 
         dictForCount = dict(collections.Counter(T))
         
-        #keep decreasing the count. if all elements from right string are found, count would be 0
         count=len(T)
         start, minSize, minStart = 0, float("inf"), 0
         
         for indx, val in enumerate((S)):
             if val in dictForCount:
-                #this will also set values to <0 when you encounter the char again 
                 dictForCount[val]-=1
                 
-                #found the element, so decrease the count
+                #-ve value would mean you are encountering the char 2nd time
                 if dictForCount[val]>=0:
                     count-=1
-            
-            #the first time reach here, all the values in count are set to 0
-            #indx is at the point where we found the last charchter we need
-            #now we have to see if the can find the chars again
             if count==0:
                 while True:
                     if S[start] in dictForCount:
+                        #this will only be negative if you encounter the char more than once
+                        #now we have to check if the could be shorter
                         if dictForCount[S[start]]<0:
                             dictForCount[S[start]]+=1
                         else:
                             break
+                    
+                    #at first, start if pointing to the 1st char in dict when we have found all the expected chars
                     #increment start it points to a char that is in the dict
                     start+=1
                 if minSize>indx-start+1:
@@ -46,4 +43,4 @@ class Solution(object):
         
 s = Solution()
 
-print(s.minWindow("dbcab","ab"))
+print(s.minWindow("dacab","ab"))
