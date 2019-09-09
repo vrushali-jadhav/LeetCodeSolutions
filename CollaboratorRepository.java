5b56664f180b2ae7d4134cd025106570ea092e66
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-09-08 17:58:43 -0700

    Delete CollaboratorRepository.java

--- CollaboratorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.management;
-
-import com.employee.management.io.entity.CollaboratorEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CollaboratorRepository extends CrudRepository<CollaboratorEntity, Long> {
-}
