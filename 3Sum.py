c325d24eca2318943f9dd979ff68ef11809abd44
Vrushali Jadhav <vjadhav@gmail.com>
2019-10-01 13:50:42 -0700

    169 using counter

---
+++ 3Sum.py
@@ -0,0 +1,11 @@
+class Solution():
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+
+s = Solution()
+print(s.threeSum([-1,0,1,2,-1,-4]))
+        
---
+++ MajorityElement.py
@@ -0,0 +1,16 @@
+import collections
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majorityNum = len(nums)/2
+        frequency = collections.Counter(nums)
+    
+        for key, val in frequency.items():
+            if val > majorityNum:
+                return key
+
+s = Solution()
+print(s.majorityElement([2,2,1,1,1,2,2]))
