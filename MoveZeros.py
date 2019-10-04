8b54f4fdb8e7c104989e3fc51a59db8096666c62
Vrushali Jadhav <vjadhav@gmail.com>
2019-10-04 15:42:26 -0700

    question 283

---
+++ MoveZeros.py
@@ -0,0 +1,21 @@
+class Solution():
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        j = len(nums)-1
+        while i < j:
+            if nums[i] == 0:
+                nums.pop(i)
+                nums.append(0)
+                j -= 1 #reduce j as you have appended an extra 0 to the end
+            else:
+                i += 1
+                
+        return nums
+
+s = Solution()
+print(s.moveZeroes([0,0,0,0,3,12])) #[3,12,0,0,0,0]
+print(s.moveZeroes([3,0,5])) #[4,2,4,3,5,1,0,0,0,0]
---
+++ tryoutRandomShit.py
@@ -0,0 +1,6 @@
+yoyo = [2,3,5,6]
+print(yoyo[-2])
+print(yoyo[2:-1])
+
+for i in range(1,4):
+    print(i)
